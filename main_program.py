e7899089ab0e6930c8ba536f5be7d14fab927a3b
alonsodaniel1090 <alonsodaniel1090@gmail.com>
2021-03-26 13:58:05 -0600

    Assign each passenger to his flight

--- main_program.py
+++ main_program.py
@@ -6,13 +6,18 @@ import requests
 def main():
     ''' Main prrogram process '''
 
+    
     passengers = get_passengers_info()
     flights  = get_flights_info()
 
-    print(flights)
+    flights_with_passengers = assign_passenger_to_flight(passengers, flights)
 
-    #for i in passengers:
-    #    print(i['ID_Pasajero'])
+
+
+
+
+
+
 
 def get_passengers_info():
     '''
@@ -43,17 +48,26 @@ def get_passengers_info():
 
 
 def get_flights_info():
+    '''
+    Get the flights information from the apis
+    provided, and returns a list with all
+    the information from the flights on
+    2016 and 2017 merged.
+    '''
 
-
+    # Set the links to extract the information
     url_flights_2016 = 'http://analytics.deacero.com/Api/GetApi/ApiVuelos2016/9ea3b836-6938-52dc-9626-a8e35db81dd5'
     url_flights_2017 = 'http://analytics.deacero.com/Api/GetApi/ApiVuelos2017/fc126260-1cf8-5a46-995d-ba639ff5868b' 
 
+    # Get the flights information from the apis
     info_2016 = requests.get(url_flights_2016)
     info_2017 = requests.get(url_flights_2017)
 
+    # Create the lists of flights
     flights_2016 = list(info_2016.json())
     flights_2017 = list(info_2017.json())
 
+    # Merge the lists
     total_flights = flights_2016 + flights_2017
     
     return total_flights
@@ -61,7 +75,26 @@ def get_flights_info():
 #url_aerolineas = 'http://analytics.deacero.com/Api/GetApi/ApiLineaAerea/1a8d9e13-ce30-50fc-bf34-6490eb799a75'
 
 
+def assign_passenger_to_flight(passengers, flights):
+    '''
+    Assign a passenger to his corresponding flight
+    and return a new dictionary will all the 
+    information for each flight.
+    '''    
 
+    # Create a copy of the original flights
+    flights_with_passengers = flights.copy()
+
+    # Look for the passenger key in each flight 
+    # and add the passenger information to it.
+    for p in passengers:
+        for f in flights_with_passengers:
+            if p['ID_Pasajero'] in f.values():    
+                f.update(p)
+
+    return flights_with_passengers
+
+    
 
 
 
